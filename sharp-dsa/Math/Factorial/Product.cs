b5bb0a40b25fe43ffa6e9fa24e56e9f8eeb5c07f
Nicolas Rocha Lacerda <nrochalacerda@gmail.com>
2026-04-17 16:49:10 -0300

    added factorial

---
+++ sharp-dsa/Math/Factorial/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace DSATraining.sharp_dsa.Math.Factorial
+{
+    public class Product
+    {
+        public int Factorial(int n)
+        {
+            if (n == 0) return 1;
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
