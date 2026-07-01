98c5b62c305621385a74b633e54f05fef4fa06d3
Nicolas Rocha Lacerda <nrochalacerda@gmail.com>
2026-06-30 21:33:06 -0300

    feat/binary search algorithm

---
+++ sharp-dsa/Algorithms/BinarySearch/BinarySearch.cs
@@ -0,0 +1,23 @@
+﻿namespace DSATraining.sharp_dsa.Algorithms.BinarySearch
+{
+    public class BinarySearch
+    {
+        public int GetIndexOf(int[] arr, int value)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while(left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if (arr[mid] == value) return mid;
+
+                if (arr[mid] < value) left = mid + 1;
+                if (arr[mid] > value) right = mid - 1;
+            }
+
+            return -1; // no index found
+        }
+    }
+}
